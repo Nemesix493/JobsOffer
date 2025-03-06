6ac3ad80c926a64101b04aa8f2e5bc4ab161b6a7
Nemesix493 <verquerre.kevin@gmail.com>
2025-03-06 15:54:29 +0100

    feat: update job_research to use JobOffer model

--- list_jobs_tech/job_research.py
+++ list_jobs_tech/job_research.py
@@ -3,7 +3,7 @@ import re
 
 from .delayed_requests import DelayedRequest
 from .job_search_website import JobSearchWebSite
-from .database import ResearchWebsite
+from .database import ResearchWebsite, JobOffer
 
 
 TEMPO_MEAN = 10
@@ -58,28 +58,6 @@ class JobResearch:
             self.get_search_pages()
         return self._search_pages
     
-    def get_offers_pages_IDs(self):
-        self._offers_pages_IDs = []
-        print(f"Load offer IDs will take {len(self.search_pages)*TEMPO_MEAN}s")
-        for search_page in self.search_pages:
-            response = tempo_requests.get(
-                search_page['url']
-            )
-            if response.ok:
-                nodes = BeautifulSoup(
-                    response.text,
-                    "html.parser"
-                ).select(search_page['targets_nodes'])
-                for node in nodes:
-                    self._offers_pages_IDs.append(node[search_page['target_data']])
-        print("Offer IDs loaded !")
-    
-    @property
-    def offers_pages_IDs(self):
-        if self._offers_pages_IDs is None:
-            self.get_offers_pages_IDs()
-        return self._offers_pages_IDs
-    
     def get_offer_url(self, offer_ID):
         return self.website_class.get_offer_url(offer_ID)
     
@@ -91,3 +69,43 @@ class JobResearch:
             response.text,
             "html.parser"
         ).select_one("div.panel-container div.modal-body div.description").text
+    
+    def get_offers_pages_IDs(self):
+        self._offers_pages_IDs = []
+        self._job_offers = []
+        print(f"Load offer IDs will take {len(self.search_pages)*TEMPO_MEAN}s")
+        for search_page in self.search_pages:
+            response = tempo_requests.get(
+                search_page['url']
+            )
+            if response.ok:
+                nodes = BeautifulSoup(
+                    response.text,
+                    "html.parser"
+                ).select(search_page['targets_nodes'])
+                for node in nodes:
+                    offer_ID = node[search_page['target_data']]
+                    self._offers_pages_IDs.append(offer_ID)
+                    self._job_offers = JobOffer(
+                        website_id=offer_ID,
+                        url=self.get_offer_url(offer_ID),
+                        description=self.get_description(offer_ID),
+                        research_website=self.website
+                    )
+        print("Offer IDs loaded !")
+    
+    @property
+    def offers_pages_IDs(self):
+        if self._offers_pages_IDs is None:
+            self.get_offers_pages_IDs()
+        return self._offers_pages_IDs
+    
+    @property
+    def job_offers(self) -> list[JobOffer]:
+        """
+        return all the offers of the research
+        as JobOffer model object
+        """
+        if self._job_offers is None:
+            self.get_offers_pages_IDs()
+        return self._job_offers
