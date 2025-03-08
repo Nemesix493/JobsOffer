a011cffa88230dd561ab5960d5bf0d3d05076041
Nemesix493 <verquerre.kevin@gmail.com>
2025-03-08 13:13:30 +0100

    refactor: refactor use iter on query

--- list_jobs_tech/report.py
+++ list_jobs_tech/report.py
@@ -2,29 +2,28 @@ from sqlalchemy import desc
 
 from .database import JobOffer, Technology, ManageDatabase
 
+
 class Report:
     @property
     def session(self):
         return ManageDatabase.get_session()
-    
+
     def report_should_learn(self):
-        return [
-            technology
-            for technology in self.session.query(Technology)
-                .filter(Technology.skill_level == 0)
-                .order_by(desc(Technology.average_score))
-                .limit(2).all()
-        ]
-    
+        return (
+            self.session.query(Technology)
+            .filter(Technology.skill_level == 0)
+            .order_by(desc(Technology.average_score))
+            .limit(2).all()
+        )
+
     def report_should_deepen(self):
-        return [
-            technology
-            for technology in self.session.query(Technology)
-                .filter(Technology.skill_level > 0)
-                .order_by(desc(Technology.deepen_score))
-                .limit(5).all()
-        ]
-    
+        return (
+            self.session.query(Technology)
+            .filter(Technology.skill_level > 0)
+            .order_by(desc(Technology.deepen_score))
+            .limit(5).all()
+        )
+
     def report_should_look(self):
         return (
             self.session
@@ -32,7 +31,7 @@ class Report:
             .order_by(desc(JobOffer.score))
             .limit(10).all()
         )
-    
+
     def report(self):
         print('You should learn :')
         for tech in self.report_should_learn():
