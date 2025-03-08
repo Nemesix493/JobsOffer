193aec7715b77ac618b38dbe85e5f8c2a5916bc9
Nemesix493 <verquerre.kevin@gmail.com>
2025-03-08 12:58:07 +0100

    feat: add method to manage database

--- list_jobs_tech/database/manage.py
+++ list_jobs_tech/database/manage.py
@@ -6,14 +6,15 @@ from sqlalchemy.orm import sessionmaker
 
 from .models import Base
 
-DB_PATH = 'sqlite:///my_db.db'
+DB_PATH = Path(__file__).resolve().parent.parent.parent / "my_db.db"
+INITIAL_DATA = Path(__file__).resolve().parent.parent.parent / "initial_data.sql"
 
 
 class ManageDatabase:
     @classmethod
     def get_engine(cls):
         if not hasattr(cls, '_engine'):
-            cls._engine = create_engine(DB_PATH)
+            cls._engine = create_engine(f'sqlite:///{DB_PATH}')
         return cls._engine
 
     @classmethod
@@ -24,20 +25,33 @@ class ManageDatabase:
 
     @classmethod
     def init_database(cls):
-        Base.metadata.create_all(bind=cls.get_engine())
-    
+        if not cls.is_db_exist():
+            if INITIAL_DATA.exists() and INITIAL_DATA.is_file():
+                cls.load_dump(INITIAL_DATA)
+            else:
+                Base.metadata.create_all(bind=cls.get_engine())
+
     @classmethod
     def destroy_database(cls):
         Base.metadata.drop_all(bind=cls.get_engine())
 
-    @classmethod
-    def dump_db(db_path: Path, dump_path: Path):
-        conn = connect(db_path)
+    @staticmethod
+    def dump_db(dump_path: Path):
+        conn = connect(DB_PATH)
         with open(dump_path, "w") as f:
             for line in conn.iterdump():
                 f.write(f"{line}\n")
         conn.close()
 
-    @classmethod
-    def load_db(db_path: Path, dump_path: Path):
-        pass
+    @staticmethod
+    def load_dump(dump_path: Path):
+        conn = connect(DB_PATH)
+        with open(dump_path, "r", encoding="utf-8") as f:
+            sql_script = f.read()
+            conn.executescript(sql_script)
+            conn.commit()
+            conn.close()
+    
+    @staticmethod
+    def is_db_exist():
+        return DB_PATH.exists() and DB_PATH.is_file()
